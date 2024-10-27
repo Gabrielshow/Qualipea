40c31f1b623f0fd7bc8fc175d919729a2c27c480
= <=>
2024-10-27 16:07:06 +0100

    add: finished the navbar off to the next section

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
       <img src={images.logo1} alt="logo" />
       </div>
       <ul className="navbar-links">
-      <li className="p-opensans"> <a href="#home">Home</a></li>
+      {/* <li className="p-opensans"> <a href="#home">Home</a></li> */}
       <li className="p-opensans"> <a href ="#about">About</a></li>
       <li className="p-opensans"> <a href = "blog">Blog</a></li>
       <li className="p-opensans"> <a href = "ContactUs">Contact Us</a></li>
