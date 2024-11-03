7237aebacbc3ae0e9a53a4476ca9fd2fb02325f2
Gabrielshow <adefelashogbanmu@gmail.com>
2024-11-03 12:54:12 +0100

    add: this should fix the error

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 export {default as Navbar} from './Navbar/Navbar';
 export { default as Separator } from './separator/separator';
-export { default as Login} from './admin/login/Login.js';
-export { default as Signin } from './admin/signin/Signin.js';
-export { default as Dashboard } from './admin/dashboard/Dashboard.js';
+export { default as Login} from './admin/login/Login';
+export { default as Signin } from './admin/signin/Signin';
+export { default as Dashboard } from './admin/dashboard/Dashboard';
